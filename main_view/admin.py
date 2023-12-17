ba48e24e6462eb0ea477fa6250a5a79dc4265314
BelekKorchuev <151909757+BelekKorchuev@users.noreply.github.com>
2023-12-17 22:59:18 +0600

    Add files via upload

--- main_view/admin.py
+++ main_view/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 from .models import OurInformations, ListOfLevels, ListOfTheme
 # Register your models here.
 
+
+class ThemeAdmin(admin.StackedInline):
+	model = ListOfTheme
+
+
+class LevelAdmin(admin.ModelAdmin):
+	inlines = [ThemeAdmin]
+
+
 admin.site.register(OurInformations)
-admin.site.register(ListOfLevels)
+admin.site.register(ListOfLevels, LevelAdmin)
 admin.site.register(ListOfTheme)
+
