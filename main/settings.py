f7da53048124fde807f18d7854143cf69f2ad9ec
BelekKorchuev <151909757+BelekKorchuev@users.noreply.github.com>
2023-12-19 21:06:32 +0600

    Add files via upload

--- main/settings.py
+++ main/settings.py
@@ -27,7 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main_view', 'lesson', 'user', 'user_profile',
-    'commenting'
+    'commenting',
 ]
 
 MIDDLEWARE = [
@@ -106,7 +106,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = [BASE_DIR / 'static']
